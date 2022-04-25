ddb8c28f5860fbfb024b32c7de7401133ea6a617
Maxingler <maxyta22@yandex.ru>
2022-04-26 00:48:17 +0300

    Added ArcherPRTData

---
+++ Source/ArcherPRT/Public/Core/ArcherPRTData.h
@@ -0,0 +1,17 @@
+// Archer Prototype. All rights reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+class ARCHERPRT_API ArcherPRTData;
+
+UENUM(BlueprintType)
+enum class EAbilityInputID : uint8
+{
+	None,
+	Confirm,
+	Cancel,
+	Punch
+};
+
