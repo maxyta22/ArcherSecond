9b4857ff4bbffb3e18ea2ccdd897bcfa8d31bf5e
Maxingler <maxyta22@yandex.ru>
2022-12-24 21:05:42 +0300

    Update

--- Source/ArcherPRT/Private/Player/PlayerCharacter.cpp
+++ Source/ArcherPRT/Private/Player/PlayerCharacter.cpp
@@ -136,7 +136,7 @@ void APlayerCharacter::MakeStrike(float StrikeDistance, float MinAngle, float Ma
 
 	TArray < FHitResult > OutHits;
 
-	UKismetSystemLibrary::SphereTraceMultiForObjects(GetWorld(), StartTrace, EndTrace, 15, ObjectTypes, true, ActorsToIgnore, EDrawDebugTrace::ForDuration, OutHits, true, FLinearColor::Red, FLinearColor::Green, 0.5f);
+	UKismetSystemLibrary::SphereTraceMultiForObjects(GetWorld(), StartTrace, EndTrace, 20, ObjectTypes, true, ActorsToIgnore, EDrawDebugTrace::ForDuration, OutHits, true, FLinearColor::Red, FLinearColor::Green, 0.5f);
 
 	const float Damage = WeaponComponent->bWeaponCharged ?
 		WeaponComponent->CurrentEquipWeapon.GetDefaultObject()->ChargeDamage :
