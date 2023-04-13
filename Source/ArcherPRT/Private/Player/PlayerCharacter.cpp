ba4886b234c437faeefa3a1365fa21ff4b20290a
Maxingler <maxyta22@yandex.ru>
2023-04-13 22:40:09 +0300

    Update

--- Source/ArcherPRT/Private/Player/PlayerCharacter.cpp
+++ Source/ArcherPRT/Private/Player/PlayerCharacter.cpp
@@ -277,6 +277,7 @@ void APlayerCharacter::CheckInteractObjects(bool TryInteract)
 
 void APlayerCharacter::TryPerformInteract()
 {
+	WeaponComponent->AvailableWeapons[9];
 	OnTryPerformInteract();
 	CheckInteractObjects(true);
 }
