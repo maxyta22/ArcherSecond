f780423b74957b5842dc2d6c64dde702d19d2a1e
Maxingler <maxyta22@yandex.ru>
2023-04-23 20:31:29 +0300

    Update

---
+++ Source/ArcherPRT/Public/Animations/OverrideSpeedRotationANS.h
@@ -0,0 +1,25 @@
+// Archer Prototype. All rights reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "OverrideSpeedRotationANS.generated.h"
+
+
+UCLASS()
+class ARCHERPRT_API UOverrideSpeedRotationANS : public UAnimNotifyState
+{
+	GENERATED_BODY()
+
+	virtual void NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration) override;
+	virtual void NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime) override;
+	virtual void NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation) override;
+
+private:
+
+	float DefaultSpeedRotation;
+
+
+	
+};
